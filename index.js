b5c265957dadf1e8afb7c196437039d22da8e866
Nahid Ahmmed Likhon <lemonnahid19@gmail>
2022-05-27 11:37:42 +0600

    access token set client site

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
@@ -57,7 +58,8 @@ async function run(){
                     
             };
             const result =await usercollections.updateOne(filter, updatedDoc, options);
-            res.send(result);
+            const token = jwt.sign({email:'email'},process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'});
+            res.send({result, token});
         })
     }
     finally{
