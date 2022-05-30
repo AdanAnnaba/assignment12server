114643e6ca42be593727aee004f099f8f2147ce7
Nahid Ahmmed Likhon <lemonnahid19@gmail>
2022-05-30 21:28:19 +0600

    new

--- index.js
+++ index.js
@@ -16,7 +16,21 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.l32d5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-
+function verifyJWT(req,res,next){
+    const authHeader = req.headers.authorization;
+    console.log(authHeader);
+    if(!authHeader){
+        return res.status(401).send({message: 'Unauthorized access'})
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err,decoded){
+        if(err){
+            return res.status(403).send({message: 'forbidden access'})
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 async function run(){
     try{
@@ -32,12 +46,17 @@ async function run(){
             res.send(product);
         })
 
-        app.get('/booking',async(req,res)=>{
+        app.get('/booking',verifyJWT, async(req,res)=>{
             const email = req.query.email;
+            const decodedEmail = req.decoded.email;
+            if(email === decodedEmail){
             const query = {email:email};
             const booking = await usersbooking.find(query).toArray();
             res.send(booking);
-            console.log(booking);
+            }
+            else{
+                return res.status(403).send({message: 'forbidden access'});
+            }
         })
 
 
@@ -48,7 +67,7 @@ async function run(){
         })
         app.post('/booking',async(req,res)=>{
             const booking = req.body;
-            const query = {email: booking.email }
+            const query = {productName: booking.productName, email: booking.email }
             const exists = await usersbooking.findOne(query);
             if(exists){
                 return res.send({success: false, productName:exists })
@@ -68,8 +87,8 @@ async function run(){
                     
             };
             const result =await usercollections.updateOne(filter, updatedDoc, options);
-            const token = jwt.sign({email:'email'},process.env.ACCESS_TOKEN_SECRET,{expiresIn:'5000h'});
-            res.send({result, token});
+            const token = jwt.sign({email:email}, process.env.ACCESS_TOKEN_SECRET,{expiresIn:'5000h'});
+            res.send({result,token});
         })
     }
     finally{
