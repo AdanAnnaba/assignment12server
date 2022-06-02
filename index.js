eec4c57860ebbad7f3b1fc5aab6ef8fb73c83f97
Nahid Ahmmed Likhon <lemonnahid19@gmail>
2022-06-02 16:25:36 +0600

    new

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ async function run(){
             return res.send({success: true, result})
         })
 
-        app.delete('/booking/:id',async(req,res)=>{
+        app.delete('/booking/:id',verifyJWT, async(req,res)=>{
                 const id = req.params.id;
                 const query = {_id: ObjectId(id)};
                 const result =await usersbooking.deleteOne(query);
